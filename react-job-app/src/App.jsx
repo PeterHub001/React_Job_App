8fefadfb766355f878eafde968172a59db4f074d
Peter <peter.ishado@gmail.com>
2025-01-06 16:29:42 +0000

    react router and pages created

--- react-job-app/src/App.jsx
+++ react-job-app/src/App.jsx
@@ -1,21 +1,20 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, } from 'react-router-dom'
 import React from "react";
-import Navbar from "./Components/Navbar";
-import Hero from "./Components/Hero";
-import HomeCards from "./Components/HomeCards";
-import JobListings from "./Components/JobListings";
-import ViewAllJobs from "./Components/ViewAllJobs";
+import HomePage from './pages/HomePage'
+import MainLayout from './layouts/MainLayout';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path='/' element={<MainLayout />}>
+    <Route index element={<HomePage />} />
+  </Route>
+  )
+);
+
+
+  
 const App = () => {
-  return <>
-  <Navbar/>
-  <Hero/>
-  <HomeCards/>
-  <JobListings/>
-  <ViewAllJobs/>
-    
-
-    
-  </>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
