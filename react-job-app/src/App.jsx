49c61f811a72a72985e11e058f84afc3e81717cf
Peter <peter.ishado@gmail.com>
2025-01-09 14:56:23 +0000

    POST and DELETE method used to query job post to ether add to api or delete from api

--- react-job-app/src/App.jsx
+++ react-job-app/src/App.jsx
@@ -6,22 +6,43 @@ import JobPages from './pages/JobPages';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-  <Route path='/' element={<MainLayout />}>
-    <Route index element={<HomePage />} />
-    <Route path='/jobs' element={<JobPages />} />
-    <Route path='/add-job' element={<AddJobPage />} />
-    <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
-    <Route path='*' element={<NotFoundPage />} />
-  </Route>
-  )
-);
-
-
   
 const App = () => {
+// Add Job to Api post request
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob)
+    });
+    return;
+
+  }
+
+  // Delete job from Api
+
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
+
+    })
+  }
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobPages />} />
+      <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+      <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />
+      <Route path='*' element={<NotFoundPage />} />
+    </Route>
+    )
+  );
+
+
   return <RouterProvider router={router} />;
 };
 
